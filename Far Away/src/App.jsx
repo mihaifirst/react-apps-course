1ee7406843e34b21e0c8ddead27690eba0909979
mihaifirst <varzaru.alexmihai@gmail.com>
2025-01-17 01:44:43 +0200

    Far Away create project

---
+++ Far Away/src/App.jsx
@@ -0,0 +1,41 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> Far Away</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have x items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
+
+export default App;
