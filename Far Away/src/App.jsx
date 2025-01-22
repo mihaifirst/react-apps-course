c88ffbfee577508e27813551969a84bb5d3ceae2
mihaifirst <varzaru.alexmihai@gmail.com>
2025-01-23 01:08:35 +0200

    derived state items

--- Far Away/src/App.jsx
+++ Far Away/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         onDeleteItems={handleDeleteItems}
         onToggleItems={handleToggleItems}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -119,12 +119,31 @@ function Item({ item, onDeleteItems, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list!!!</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have x items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything packed"
+          : `  You have ${numItems} ${
+              numItems === 1 ? "item" : "items"
+            } on your list,
+        and you already packed ${numPacked} (${percentage}%) `}
+      </em>
     </footer>
   );
+  dsadsadsa;
 }
 
 export default App;
